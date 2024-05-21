4b751291f643238e5b93979e4056b1c6c7b39170
김준호 <taotrpmar@gmail.com>
2024-05-22 04:40:47 +0900

    0522

---
+++ wargame/dreamhack/crypto/level2/padding_miracle_attack/solved.py
@@ -0,0 +1,6 @@
+from pwn import *
+
+p=remote('host3.dreamhack.games', 23703)
+
+
+#ncat host3.dreamhack.games 23703
--- wargame/dreamhack/crypto/level4/textbook-cbc/solved.py
+++ wargame/dreamhack/crypto/level4/textbook-cbc/solved.py
@@ -1,6 +1,8 @@
 from pwn import *
+from Crypto.Cipher import AES
+from Crypto.Util.Padding import unpad
 
-p=remote('host3.dreamhack.games', 20025)
+p=remote('host3.dreamhack.games', 14845)
 
 def enc(pt):
     p.sendline(b'1')
@@ -14,15 +16,24 @@ def dec(ct):
     p.sendline(ct.encode())
     return p.recvline()[:-1].decode()
 
+def hexxor(a, b):
+    return hex(int(a, 16)^int(b, 16))[2:]
+
 p.recv(1024)
 zero='0000000000000000000000000000000000000000000000000000000000000000'
-random_value='11'
+random_value='11111111111111111111111111111111'
 enc_random_value=enc(random_value)
 zr=zero+enc_random_value
-print(zr)
+dec_zr=dec(zr)
+IV=hexxor(dec_zr[0:32], dec_zr[32:64])
+IV=bytes.fromhex(IV)
+p.sendline(b'3')
+p.recvuntil(b'= ')
+enc_flag=bytes.fromhex(p.recvline()[:-1].decode())
+flag=AES.new(IV, AES.MODE_CBC, IV).decrypt(enc_flag)
+print(flag)
+flag=unpad(flag, 16).decode()
+print(flag)
 
 
 #ncat host3.dreamhack.games 10116
-
-#'00000000000000000000000000000000'
-#'0000000000000000'
