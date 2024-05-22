8137acc851c023bcd6f178b8cbdf300adac6bd67
김준호 <taotrpmar@gmail.com>
2024-05-23 01:37:10 +0900

    0523

--- wargame/dreamhack/crypto/level2/padding_miracle_attack/solved.py
+++ wargame/dreamhack/crypto/level2/padding_miracle_attack/solved.py
@@ -1,6 +1,33 @@
 from pwn import *
+from os import *
 
-p=remote('host3.dreamhack.games', 23703)
+p=remote('host3.dreamhack.games', 17573)
+p.recvuntil(b': ')
+p.sendline(b'1')
+p.recvuntil(b': ')
+p.sendline(b'secret')
+p.recvuntil(b'=> ')
+s=bytes.fromhex(p.recvline()[:-1].decode())
 
 
-#ncat host3.dreamhack.games 23703
+def enc(pt):
+    p.sendline(b'1')
+    p.recvuntil(b': ')
+    p.sendline(bytes.hex(pt).encode())
+    p.recvuntil(b'=> ')
+    return bytes.fromhex(p.recvline()[:-1].decode())
+
+def dec(ct):
+    p.sendline(b'2')
+    p.recvuntil(b': ')
+    p.sendline(bytes.hex(ct).encode())
+    p.recvuntil(b'=> ')
+    msg=p.recvline()[:-1].decode()
+    if(msg=="Don't steal my secret!"):
+        return True
+    else:
+        return False
+
+
+
+#ncat host3.dreamhack.games 17573
