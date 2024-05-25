8b329f27aa175504909b4751fc9f1d652e44c47a
김준호 <taotrpmar@gmail.com>
2024-05-25 14:42:26 +0900

    0524

--- wargame/dreamhack/crypto/level2/padding_miracle_attack/solved.py
+++ wargame/dreamhack/crypto/level2/padding_miracle_attack/solved.py
@@ -1,13 +1,13 @@
 from pwn import *
 from os import *
 
-p=remote('host3.dreamhack.games', 9554)
+p=remote('host3.dreamhack.games', 20292)
 p.recvuntil(b': ')
 p.sendline(b'1')
 p.recvuntil(b': ')
 p.sendline(b'secret')
 p.recvuntil(b'=> ')
-s=bytes.fromhex(p.recvline()[:-1].decode())
+es=bytes.fromhex(p.recvline()[:-1].decode())
 
 
 def enc(pt):
@@ -28,20 +28,30 @@ def dec(ct):
     else:
         return False
 
+def submit(msg):
+    msg = bytes.hex(msg)
+    p.sendline(b"3")
+    p.sendline(msg.encode())
+    p.recvuntil(b"secret: ")
+
 def p_attack(ct):
     zero=[0]*16
     for i in range(16):
         ex1=zero[:]
         for j in range(i):
             ex1[15-j]=ex1[15-j]^(i+1)
-        print(f"ex1 : {ex1}")
         for k in range(256):
             ex1[15-i]=k
             if(dec(bytes(ex1)+ct)):
                 break
         zero[15-i]=k^(i+1)
-        print(zero)
-    return
-a=b'ahahah'
-print(p_attack(enc(a)))
-#ncat host3.dreamhack.games 15786
+    return bytes(zero)
+
+IV_secret=p_attack(es)
+IV=p_attack(enc(bytes(16))[:16])
+
+submit(xor(IV, IV_secret))
+p.interactive()
+
+
+#ncat host3.dreamhack.games 20292
